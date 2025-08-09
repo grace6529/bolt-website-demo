df29bcc85e9a6d17c8d747fabed4c7ee31c8bbc5
grace6529 <grace@prettysim.pl>
2025-08-09 19:38:42 +0200

    Fix whatToExpected variable typo

--- src/pages/DiscoveryCallPage.tsx
+++ src/pages/DiscoveryCallPage.tsx
@@ -169,7 +169,7 @@ const DiscoveryCallPage = () => {
               <div>
                 <h4 className="font-semibold text-gray-900 mb-3">We'll Cover:</h4>
                 <ul className="space-y-2">
-                  {whatToExpected.map((item, index) => (
+                  {whatToExpect.map((item, index) => (
                     <li key={index} className="flex items-start space-x-2">
                       <CheckCircle className="h-5 w-5 text-green-500 flex-shrink-0 mt-0.5" />
                       <span className="text-gray-700">{item}</span>
